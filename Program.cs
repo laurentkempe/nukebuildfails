4d8d2009f638aeec903c43dbe372e77cc3ad3aea
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2021-04-26 16:28:25 +0200

    Add initial compiling project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nukebuildfails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Strings.Program_Main_Hello_World);
+        }
+    }
+}
